3bd6cf3131de031924b7d11e82cffc00a19b0065
olesgedz <olesgedz@rambler.ru>
2019-05-09 23:17:45 +0300

    sdd

--- ex00/ex00.php
+++ ex00/ex00.php
@@ -21,7 +21,45 @@
 				echo "\n";
 			}
 		}
+		public function setOnMap($ships)
+		{
+			foreach($ships as $ship)
+			{
+				$this->data[$ship->position->x + $ship->position->y * $this->width] = $ship->_body;
+			}
+		}
+	}
+	class Point
+	{
+		public $x;
+		public $y;
+
+		function Point($x, $y)
+		{
+			$this->x = $x;
+			$this->y = $y;
+		}
+	}
+	class Ship
+	{
+		public $position;
+		public $_body = 'x';
+		function Ship(Point $point)
+		{
+			$this->position = $point;
+		}
+		public function moveby($x, $y)
+		{
+			$this->position->x += $x;
+			$this->position->y += $y;
+		}
 	}
 
 	$map = new Map(10, 10);
+	$map->print_map();
+	
+	$ships[] = new Ship(new Point(1, 1));
+	echo "\n\n";
+	$ships[0]->moveby(1,1);
+	$map->setOnMap($ships);
 	$map->print_map();
